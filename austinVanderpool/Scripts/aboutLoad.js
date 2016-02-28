07968968a1e48b2640a065facc44642db0cdb28e
Austin-Vanderpool <austinvanderpool@gmail.com>
2016-02-28 01:38:36 -0500

    first

---
+++ austinVanderpool/Scripts/aboutLoad.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $('#compSci').click(function () {
+        $('.bussContent').hide();
+        $('.aboutWrapper').fadeOut("fast");
+        $('.aboutWrapper').fadeIn("slow");
+        $.ajax({
+            url: "/Home/compSci",
+            dataType: 'html',
+            type: "GET",
+            success: function (data) {
+                $('.bussContent').html(data);
+                $('.bussContent').fadeIn('slow');
+            }
+        });
+    });
+
+    $('#bioChem').click(function () {
+        $('.bussContent').hide();
+        $('.aboutWrapper').fadeOut("fast");
+        $('.aboutWrapper').fadeIn("slow");
+        $.ajax({
+            url: "/Home/bioChem",
+            dataType: 'html',
+            type: "GET",
+            success: function (data) {
+                $('.bussContent').html(data);
+                $('.bussContent').fadeIn('slow');
+            }
+        });
+    });
+
+})
